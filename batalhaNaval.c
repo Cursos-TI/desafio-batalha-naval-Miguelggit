3ffb92244bf808f99ba3c69d63239c86d08030d4
Miguelggit <contato.miguel222@gmail.com>
2025-08-11 21:37:27 -0300

    Primeira logica do nivel avançado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 
+#define linha 10
+#define coluna 10
+#define tiroConeX 4
+#define tiroConeY 4
+#define tiroCruzX 7
+#define tiroCruzY 7
+#define tiroOctaedroX 8
+#define tiroOctaedroY 2
+
 int main(){
-        int tabuleiro[10][10] = {0};
+
+        int tabuleiro[linha][coluna] = {0};
+
         int linha_tabuleiro = 7;
         int coluna_tabuleiro = 7;
+
         int linha_tabuleiro2 = 0;
         int coluna_tabuleiro2 = 4;
+
         int linha_tabuleiro3 = 0;
         int coluna_tabuleiro3 = 0;
+
         int linha_tabuleiro4 = 5;
         int coluna_tabuleiro4 = 3;
+
+        int cone[tiroConeX][tiroConeY];
+        int cruz[tiroCruzX][tiroCruzY];
+        int octaedro[tiroOctaedroX][tiroOctaedroY];
         
             for(size_t l = 1; l <= 3; l++){
                 tabuleiro[linha_tabuleiro][coluna_tabuleiro++] = 3; // Navios representados em Horizontal
@@ -18,11 +36,24 @@ int main(){
                 tabuleiro[linha_tabuleiro4++][coluna_tabuleiro4++] = 3; // Navios representados em Diagonal 
             }
 
-        for(size_t i = 0; i <= 9; i++ ){
+        for(size_t i = 0; i < linha; i++ ){
 
-                        for(size_t j = 0; j <= 9; j++){
+                        for(size_t j = 0; j < coluna; j++){
 
-                            printf("%d ",tabuleiro[i][j]);
+                            if(tiroConeX == i && tiroConeY == j){
+
+                                tabuleiro[i][j] = 1; // 1 Representa locais acertados
+                                printf("%d ",tabuleiro[i][j]);
+                                
+                            } else if (tiroCruzX == i && tiroCruzY == j){
+                                tabuleiro[i][j] = 1;
+                                printf("%d ",tabuleiro[i][j]);
+                            } else if(tiroOctaedroX == i & tiroOctaedroY == j){
+                                tabuleiro[i][j] = 1;
+                                printf("%d ",tabuleiro[i][j]);    
+                            } else{
+                                printf("%d ",tabuleiro[i][j]);
+                            }
 
                         }
         printf("\n");
